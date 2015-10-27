02cdb76527f799f022cab83d0db390b3ac190691
kypdurron <kyp@durron.net>
2015-10-27 21:19:20 +0100

    Added : Starting NUnit Test Implementation Added : Starting Zone Geometry Implementation

---
+++ DaocClientLib.Test/ClientDataWrapperTest.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * DAWN OF LIGHT - The first free open source DAoC server emulator
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+using System;
+using System.IO;
+
+using DaocClientLib;
+
+using NUnit.Framework;
+
+namespace DaocClientLib.Test
+{
+	/// <summary>
+	/// ClientDataWrapperTest NUNIT Test Class
+	/// </summary>
+	[TestFixture]
+	public class ClientDataWrapperTest
+	{
+		/// <summary>
+		/// An Inexisting Path to Test
+		/// </summary>
+		public const string WrongClientPath = @"ZZ:\SomeWrongDir";
+		
+		/// <summary>
+		/// Test Loading a null directory for Argument Exception
+		/// </summary>
+		[Test]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void TestWrongDirectoryLoading()
+		{
+			string str = null;
+			var test = new ClientDataWrapper(str);
+		}
+		
+		/// <summary>
+		/// Test Loading an inexisting directory Path for File Exception
+		/// </summary>
+		[Test]
+		[ExpectedException(typeof(FileNotFoundException))]
+		public void TestWrongPathDirectoryLoading()
+		{
+			var test = new ClientDataWrapper(WrongClientPath);
+		}
+		
+		public ClientDataWrapperTest()
+		{
+		}
+	}
+}
